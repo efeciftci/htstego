372b46a6f8a5674de8ef46500d62b24493884997
Efe Çiftci <efeciftci@gmail.com>
2023-07-25 17:28:53 +0300

    seperated errdiffbin from main for easily implementing future methods

--- libhtstego.py
+++ libhtstego.py
@@ -1,4 +1,5 @@
 import numpy as np
+from skimage import io, metrics
 
 
 def floyd(I):
@@ -102,3 +103,80 @@ def snr(o, n):
     ps = np.mean(o**2)
     pn = np.mean((o - n)**2)
     return 10 * np.log10(ps / pn)
+
+
+def htstego_errdiffbin(NSHARES=4,
+                       imparam='airplane80',
+                       txtparam=512,
+                       errdiffmethod='floyd',
+                       nooutput=False):
+    errdifffun = globals().get(errdiffmethod)
+    imfile = f'{imparam}_256gray'
+    impath = f'cover_imgs/{imfile}.png'
+    I = io.imread(impath, as_gray=True) / 255.0
+    M, N = I.shape
+
+    txtfile = f'payloads/payload{txtparam}.txt'
+    messageAscii = open(txtfile).read()
+    messageBinary = ''.join(format(ord(c), '08b') for c in messageAscii)
+
+    blockSize = I.size // len(messageBinary)
+    if blockSize == 0:
+        print(f'[{NSHARES:2d} {imparam:9s} {txtparam:4d}] message too long!')
+        return
+
+    normalOutput = errdifffun(I)  # floyd(I), fan(I)
+    stegoOutputs = np.zeros((I.shape[0], I.shape[1], NSHARES))
+    linearImage = normalOutput.reshape(1, -1)[0]
+    linearStegoImages = np.tile(linearImage, (NSHARES, 1))
+
+    snrs = np.zeros((NSHARES, 2))
+    ssims = np.zeros((NSHARES, 1))
+
+    messagePos = 0
+    for i in range(0, M * N, blockSize):
+        if messagePos < len(messageBinary):
+            stegoPixel = int(messageBinary[messagePos])
+
+            if i + blockSize - 1 > M * N:
+                print(i + blockSize - 1)
+                break
+
+            currentBlock = linearImage[i:i + blockSize]
+            stegoBlock = currentBlock.copy()
+
+            embedHere = findEmbedPositionErrDiff(currentBlock, stegoPixel)
+            if embedHere == -1:
+                continue
+
+            stegoBlock[embedHere] = stegoPixel
+            randomShare = np.random.randint(0, NSHARES)
+            linearStegoImages[randomShare, i:i + blockSize] = stegoBlock
+            messagePos += 1
+        else:
+            break
+
+    normalOutput = (normalOutput * 255).astype(np.uint8)
+
+    if nooutput == False:
+        normalOutputPath = f'output/{imfile}_hterrdiffbin_regular_{errdiffmethod}.png'
+        io.imsave(normalOutputPath, normalOutput)
+        stegoOutputPaths = []
+
+    for i in range(NSHARES):
+        stegoOutputs[:, :, i] = linearStegoImages[i, :].reshape(M, N)
+        stegoImage = (stegoOutputs[:, :, i] * 255).astype(np.uint8)
+        if nooutput == False:
+            stegoOutputPaths.append(
+                f'output/{imfile}_hterrdiffbin_stego_msg{txtparam}_{i+1}of{NSHARES}_{errdiffmethod}.png'
+            )
+            io.imsave(stegoOutputPaths[i], stegoImage)
+
+        snrs[i, 0] = snr(normalOutput, stegoImage)
+        snrs[i, 1] = metrics.peak_signal_noise_ratio(stegoImage, normalOutput)
+        ssims[i] = metrics.structural_similarity(stegoImage, normalOutput)
+
+    avg_snr = np.mean(snrs[:, 0])
+    avg_psnr = np.mean(snrs[:, 1])
+    avg_ssim = np.mean(ssims)
+    return avg_snr, avg_psnr, avg_ssim
--- main.py
+++ main.py
@@ -1,95 +1,22 @@
 #!/usr/bin/env python3
 import argparse
-import numpy as np
-from skimage import io, metrics
 from libhtstego import *
 
 VERSION = '0.0.2'
 
-
-def htstego_errdiffbin(NSHARES=4,
-                       imparam='airplane80',
-                       txtparam=512,
-                       errdiffmethod='floyd',
-                       nooutput=False):
-    errdifffun = globals().get(errdiffmethod)
-    imfile = f'{imparam}_256gray'
-    impath = f'cover_imgs/{imfile}.png'
-    I = io.imread(impath, as_gray=True) / 255.0
-    M, N = I.shape
-
-    txtfile = f'payloads/payload{txtparam}.txt'
-    messageAscii = open(txtfile).read()
-    messageBinary = ''.join(format(ord(c), '08b') for c in messageAscii)
-
-    blockSize = I.size // len(messageBinary)
-    if blockSize == 0:
-        print(f'[{NSHARES:2d} {imparam:9s} {txtparam:4d}] message too long!')
-        return
-
-    normalOutput = errdifffun(I)  # floyd(I), fan(I)
-    stegoOutputs = np.zeros((I.shape[0], I.shape[1], NSHARES))
-    linearImage = normalOutput.reshape(1, -1)[0]
-    linearStegoImages = np.tile(linearImage, (NSHARES, 1))
-
-    snrs = np.zeros((NSHARES, 2))
-    ssims = np.zeros((NSHARES, 1))
-
-    messagePos = 0
-    for i in range(0, M * N, blockSize):
-        if messagePos < len(messageBinary):
-            stegoPixel = int(messageBinary[messagePos])
-
-            if i + blockSize - 1 > M * N:
-                print(i + blockSize - 1)
-                break
-
-            currentBlock = linearImage[i:i + blockSize]
-            stegoBlock = currentBlock.copy()
-
-            embedHere = findEmbedPositionErrDiff(currentBlock, stegoPixel)
-            if embedHere == -1:
-                continue
-
-            stegoBlock[embedHere] = stegoPixel
-            randomShare = np.random.randint(0, NSHARES)
-            linearStegoImages[randomShare, i:i + blockSize] = stegoBlock
-            messagePos += 1
-        else:
-            break
-
-    normalOutput = (normalOutput * 255).astype(np.uint8)
-
-    if nooutput == False:
-        normalOutputPath = f'output/{imfile}_hterrdiffbin_regular_{errdiffmethod}.png'
-        io.imsave(normalOutputPath, normalOutput)
-        stegoOutputPaths = []
-
-    for i in range(NSHARES):
-        stegoOutputs[:, :, i] = linearStegoImages[i, :].reshape(M, N)
-        stegoImage = (stegoOutputs[:, :, i] * 255).astype(np.uint8)
-        if nooutput == False:
-            stegoOutputPaths.append(
-                f'output/{imfile}_hterrdiffbin_stego_msg{txtparam}_{i+1}of{NSHARES}_{errdiffmethod}.png'
-            )
-            io.imsave(stegoOutputPaths[i], stegoImage)
-
-        snrs[i, 0] = snr(normalOutput, stegoImage)
-        snrs[i, 1] = metrics.peak_signal_noise_ratio(stegoImage, normalOutput)
-        ssims[i] = metrics.structural_similarity(stegoImage, normalOutput)
-
-    avg_snr = np.mean(snrs[:, 0])
-    avg_psnr = np.mean(snrs[:, 1])
-    avg_ssim = np.mean(ssims)
-
-    print(
-        f'[erdbin] [{NSHARES:2d} {imparam:10s} {txtparam:4d}] [avg_snr: {avg_snr:.4f}] [avg_psnr: {avg_psnr:.4f}] [avg_ssim: {avg_ssim:.4f}]'
-    )
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=f'Halftone steganography utility version {VERSION}')
+    parser.add_argument(
+        '--htmethod',
+        type=str,
+        default='errdiff',
+        help='halftoning method (pattern, errdiff) (default=errdiff)')
+    parser.add_argument(
+        '--htmethodtype',
+        type=str,
+        default='binary',
+        help='halftoning type (binary, color) (default=binary)')
     parser.add_argument('--nshares',
                         type=int,
                         default=4,
@@ -111,8 +38,16 @@ if __name__ == '__main__':
                         action='store_true',
                         help='do not produce output images (default=false)')
     args = parser.parse_args()
-    htstego_errdiffbin(NSHARES=args.nshares,
-                       imparam=args.imparam,
-                       txtparam=args.txtparam,
-                       errdiffmethod=args.method,
-                       nooutput=args.no_output)
+
+    if args.htmethod == 'errdiff' and args.htmethodtype == 'binary':
+        [avg_snr, avg_psnr,
+         avg_ssim] = htstego_errdiffbin(NSHARES=args.nshares,
+                                        imparam=args.imparam,
+                                        txtparam=args.txtparam,
+                                        errdiffmethod=args.method,
+                                        nooutput=args.no_output)
+        print(
+            f'[erdbin] [{args.nshares:2d} {args.imparam:10s} {args.txtparam:4d}] [avg_snr: {avg_snr:.4f}] [avg_psnr: {avg_psnr:.4f}] [avg_ssim: {avg_ssim:.4f}]'
+        )
+    else:  #errdiffcol, patbin, patcol
+        print('Not yet implemented!')
